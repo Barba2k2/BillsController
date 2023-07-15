414066fa41127407d536415889300669917e44be
Barba2k2 <lorenzo.dz@hotmail.com>
2023-07-15 13:35:48 -0300

    ADD: Tests on Middleware and Transactions

--- backend/transactions/routes.js
+++ backend/transactions/routes.js
@@ -1,11 +1,17 @@
 import express from "express";
+import admin from "firebase-admin";
 import { authenticateToken } from "../middlewares/authenticate-jwt.js";
 import { TransactionController } from "../transactions/controller.js";
 
-const app = express()
+const app = express();
 
 const transactionController = new TransactionController();
 
-app.get("/", authenticateToken, transactionController.findByUser);
+app.get(
+  "/",
+  (request, response, next) =>
+    authenticateToken(request, response, next, admin.auth()),
+  (request, response) => transactionController.findByUser(request, response)
+);
 
-export const transactionsRouter = app
+export const transactionsRouter = app;
